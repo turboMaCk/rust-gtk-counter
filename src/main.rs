22cb7253a5fdb537df7f07863da6c92932e65e01
Marek Fajkus <marek.faj@gmail.com>
2019-07-28 19:22:24 +0200

    Introduce state management

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,28 @@ use gtk::prelude::*;
 use gio::prelude::*;
 use gtk::{Application, ApplicationWindow, Button, Box, Label,};
 
+use std::sync::atomic::{AtomicIsize, Ordering};
+
+struct Counter(AtomicIsize);
+
+impl Counter {
+    fn new(init: isize) -> Counter {
+        Counter(AtomicIsize::new(init))
+    }
+
+    fn increment(&self) -> isize {
+        let new = self.0.load(Ordering::SeqCst) + 1;
+        self.0.store(new, Ordering::SeqCst);
+        new
+    }
+
+    fn decrement(&self) -> isize {
+        let new = self.0.load(Ordering::SeqCst) - 1;
+        self.0.store(new, Ordering::SeqCst);
+        new
+    }
+}
+
 fn main() {
     // The simplest way to initalize new application
     let application = Application::new(None, Default::default())
@@ -22,6 +44,10 @@ fn main() {
         // Containers
         let container = Box::new(gtk::Orientation::Vertical, 3);
 
+        // State
+        use std::sync::Arc;
+        let counter = Arc::new(Counter::new(0));
+
         // Header
         let header = gtk::HeaderBar::new();
         let label = Label::new(Some("Starting at 0"));
@@ -30,15 +56,19 @@ fn main() {
         let inc_btn = Button::new_with_label("Increment");
         {
             let label_clone = label.clone();
+            let counter_clone = counter.clone();
             inc_btn.connect_clicked(move |_| {
-                label_clone.set_label("Incremented");
+                let val = counter_clone.increment();
+                label_clone.set_label(&format!("Incremented to {}", val));
             });
         }
         let dec_btn = Button::new_with_label("Decrement");
         {
             let label_clone = label.clone();
+            let counter_clone = counter.clone();
             dec_btn.connect_clicked(move |_| {
-                label_clone.set_label("Decremented");
+                let val = counter_clone.decrement();
+                label_clone.set_label(&format!("Decremented to {}", val));
             });
         }
 
